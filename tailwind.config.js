677127592126d73eff638cc8693326a85cf29f74
brendt <brent.roose@gmail.com>
2024-02-15 06:08:35 +0100

    Project setup

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./app/**/*.{view.php,html,js}"
+    ],
+    theme: {
+        extend: {},
+    },
+    mode: 'jit',
+    plugins: [],
+}
+
