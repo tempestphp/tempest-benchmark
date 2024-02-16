b92aaa21893869497ad717e54fc2ee75b236cd6e
brendt <brent.roose@gmail.com>
2024-02-16 06:31:32 +0100

    Add setup

--- setup/ConsoleCommand_001.php
+++ setup/ConsoleCommand_001.php
@@ -6,25 +6,25 @@ use Tempest\Console\ConsoleCommand;
 
 final readonly class ConsoleCommand_001
 {
-    #[ConsoleCommand('001:a')]
+    #[ConsoleCommand('c_001:a')]
     public function a_001()
     {
 
     }
 
-    #[ConsoleCommand('001:b')]
+    #[ConsoleCommand('c_001:b')]
     public function b_001()
     {
 
     }
 
-    #[ConsoleCommand('001:c')]
+    #[ConsoleCommand('c_001:c')]
     public function c_001()
     {
 
     }
 
-    #[ConsoleCommand('001:d')]
+    #[ConsoleCommand('c_001:d')]
     public function d_001()
     {
 
