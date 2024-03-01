28429f366b4da4807cb26b7263cb892e88df01ae
brendt <brent.roose@gmail.com>
2024-03-01 06:24:59 +0100

    Update

--- public/index.php
+++ public/index.php
@@ -1,18 +1,9 @@
 <?php
 
-use Tempest\AppConfig;
-use Tempest\Application\Environment;
 use Tempest\Tempest;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-Tempest::setupEnv(__DIR__ . '/../');
-
-Tempest::boot(new AppConfig(
-    appPath: __DIR__ . '/../app',
-    appNamespace: 'App\\',
-    environment: Environment::from(getenv('ENVIRONMENT')),
-    discoveryCache: getenv('DISCOVERY_CACHE'),
-))->http()->run();
+Tempest::boot(__DIR__ . '/../')->http()->run();
 
 exit;
